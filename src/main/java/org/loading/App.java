60dafd6b31c516323ff46c88e7aa50b1589841a0
loading <lm380428885@gmail.com>
2022-05-24 11:28:35 +0800

    first commit

---
+++ src/main/java/org/loading/App.java
@@ -0,0 +1,13 @@
+package org.loading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
